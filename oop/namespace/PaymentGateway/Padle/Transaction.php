82651a7010639afa83e09310160c8ed54e86f749
AbdessamadSayhi <abdessamadsayhi17@gmail.com>
2023-11-30 18:35:01 +0100

    initializing 2 classes with same name using namespace

--- oop/namespace/PaymentGateway/Padle/Transaction.php
+++ oop/namespace/PaymentGateway/Padle/Transaction.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 /* when define a function, constant or a class without a namespace definition, 
 by default they will be put in a global space 
 (like this example the Transaction class is put in a global space)*/
+
+namespace PaymentGateway\Padle;
+
 class Transaction
 {
     public function __construct()
---
+++ oop/namespace/index.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+require_once('PaymentGateway/Stripe/Transaction.php');
+require_once('PaymentGateway/Padle/Transaction.php');
+
+/*
+When you require 2 classes with the same name
+and they have not namespaced(meaning they are in the global space),
+this will cause an error (Fatal error: Cannot declare class Transaction, because the name is already in use in )
+*/
+// this will no longer work because PHP cannot desice which class to use.
+//var_dump(new Transaction());
+
+// instead we will have to use the namespace when declaring the class.
+var_dump(new \PaymentGateway\Padle\Transaction());
